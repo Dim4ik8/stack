f2f329b4d243d78c55e7a7de70b0639d41b7c91a
Zhlobo Dmitry <dmitry.zhlobo@yandex.ru>
2021-11-20 14:27:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+class Stack:
+    elements = []
+
+    def __init__(self):
+        self.elements = []
+
+    def is_empty(self):
+        if len(self.elements):
+            return True
+        else:
+            return False
+
+    def push(self, element):
+        self.elements.insert(0, element)
+
+    def pop(self):
+        self.elements.remove(self.peek())
+        return self.elements
+
+    def peek(self):
+        return self.elements[0]
+
+    def size(self):
+        return len(self.elements)
+
+
+class BracketsStack(Stack):
+    opening_brackets = '[{('
+    closing_brackets = ']})'
+
+    def __init__(self, sequence: str):
+        for bracket in sequence:
+            self.push(bracket)
+        if self.is_balanced():
+            print('OK\t|\tСтрока сбалансирована.')
+        else:
+            print('WARNING\t|\tСтрока не сбалансирована!')
+
+    def peek_is_opening(self):
+        # Проверяет является ли последняя скобка в стеке открывающейся
+        if self.peek() in self.opening_brackets:
+            return True
+        else:
+            return False
+
+    def peek_is_equal(self, element):
+        # Проверяет закрывает ли новая скобка предыдущую
+        if element == ']' and self.peek() == '[':
+            return True
+        elif element == '}' and self.peek() == '{':
+            return True
+        elif element == ')' and self.peek() == '(':
+            return True
+        else:
+            return False
+
+    def push(self, element):
+        # Добавляем доп проверки в родительский метод
+        if element in self.opening_brackets:
+            super().push(element)
+        elif element not in self.opening_brackets and element not in self.closing_brackets:
+            pass
+        elif element in self.closing_brackets:
+            if self.peek_is_opening() and self.peek_is_equal(element):
+                # Если скобка закрывается, и она того же типа что и предыдущая, то предыдущая просто удаляется
+                super().pop()
+
+    def is_balanced(self):
+        if self.size():
+            return False
+        else:
+            return True
+
+
+stack = Stack()
+
+stack.push('2')
+stack.push('3')
+stack.push('4')
+# stack.push('abc')
+print(stack.elements)
+stack.pop()
+print(stack.elements)
+
+
+bracket_stack = BracketsStack('({[([[{}]]))')
+print(bracket_stack.size())
